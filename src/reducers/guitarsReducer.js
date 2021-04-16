a7ae95a1d15ecb610129693888d2b5d70e437c78
MichaelAbe <michael.k.abe@gmail.com>
2021-04-16 16:41:17 -0400

    added guitars reducer set inital state

---
+++ src/reducers/guitarsReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    guitars: [],
+    loading: true 
+}
+
+const guitarsReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
