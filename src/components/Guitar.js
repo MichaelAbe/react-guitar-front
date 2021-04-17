d9cc10ea4bda58aeb3dc9b6d4814d7ff32cd9566
MichaelAbe <michael.k.abe@gmail.com>
2021-04-16 21:14:52 -0400

    added guitar component and basic setup

---
+++ src/components/Guitar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Guitar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Guitar Component</h3>
+            </div>
+        )
+    }
+}
+
+export default Guitar
