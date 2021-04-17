220a1a394977ce4423dcb2b6a286c1591a3b4d68
MichaelAbe <michael.k.abe@gmail.com>
2021-04-16 21:19:20 -0400

    added err component and basic setup

---
+++ src/components/Err.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Err extends Component {
+    render() {
+        return (
+            <div>
+                <h3>You have run into an ERROR!</h3>
+            </div>
+        )
+    }
+}
+
+export default Err
