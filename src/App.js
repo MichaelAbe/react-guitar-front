6416153fc0b8efad9de8932ae79ebc9021524798
MichaelAbe <michael.k.abe@gmail.com>
2021-04-16 21:24:20 -0400

    starting routing added basic home component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import react, { Component} from 'react'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 // import './App.css';
 import HelloWorld from './components/HelloWorld';
+import Nav from './components/Nav'
 
 class App extends Component {
   
   render() {
     return (
+      <Router>
+        <Nav /> 
       <div className="App">
         <HelloWorld />
         <h1>This is a test</h1>
       </div>
+      </Router>
     );
 
   }
