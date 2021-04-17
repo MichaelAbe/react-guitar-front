4d5bd69cc3bdb4c2f281cf3dd9ba9b4abe663329
MichaelAbe <michael.k.abe@gmail.com>
2021-04-17 11:01:44 -0400

    guitar addGuitar action

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,5 +7,21 @@ export const getGuitars = () => {
     }
 }
 
-
   
+export const addGuitar = (guitar, history) => {
+    return dispatch => {
+      fetch('http://localhost:3001/guitars', {
+        method: "POST",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ guitar })
+      })
+        .then(response => response.json())
+        .then(guitar => {
+          dispatch({ type: "ADD_GUITAR", guitar })
+          history.push("/guitars")
+        })
+    }
+  }
